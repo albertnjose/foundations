7f65fbaa0713ff6aa509aeb06f5b5ec8db280cc5
Albert <albertnjose@gmail.com>
2016-12-22 15:16:40 -0800

    Revised to take sentence and loop through multiple inputs

--- piglatin.py
+++ piglatin.py
@@ -2,29 +2,39 @@
 
 def pig_trans():
     vowels = ['a', 'e', 'i', 'o', 'u']
-    user_word = input('Type any word: ')
-    
-    if(len(user_word) > 0 and user_word.isalpha()): # checks for nonempty word and
-        # has alphabet characters
-        lower_word = user_word.lower()
-    # if word starts with a vowel append 'ay' otherwise continue to consonants logic
+
+    while True:
+        sentence_new = []
+        sentence = input("Type any sentence: ")
+        sentence = sentence.split(' ')
         
-        begin_with = lower_word[0]
-        if begin_with in (vowels):
-            pig_word = lower_word + 'ay'
-            print(pig_word)
-    # checks for multiple consonants creates prefix    
-        else:
-            add_prefix = ''
-            for x in range(len(lower_word)):
-                if lower_word[x] in vowels:
-                    break
+        for word in sentence:
+            if(word.isalpha()): # checks for nonempty word and
+                # has alphabet characters
+                lower_word = word.lower()
+
+                # if word starts with a vowel append 'ay' otherwise continue to consonants logic
+                begin_with = lower_word[0]
+                if begin_with in (vowels):
+                    pig_word = lower_word + 'way'
+                    print(pig_word)
+                    
+                # checks for multiple consonants creates prefix    
                 else:
-                    add_prefix = add_prefix + lower_word[x]
-    
-            pig_word = lower_word[len(add_prefix):] + add_prefix + 'ay'
-            print(pig_word)
-    else:
-        print('Not a valid word.')
+                    add_prefix = ''
+                    for x in range(len(lower_word)):
+                        if lower_word[x] in vowels:
+                            break
+                        else:
+                            add_prefix = add_prefix + lower_word[x]
+            
+                    pig_word = lower_word[len(add_prefix):] + add_prefix + 'ay'
+                    print(pig_word)
+            else:
+                print('Not a valid word.')
+
+            sentence_new.append(pig_word)
+
+        print(sentence_new)
         
 pig_trans()    
